b960ca6eac4e3f3d4c101c4e038b188c5afc28ec
Dmitry Kukolev <glarosh@gmail.com>
2022-12-19 16:43:00 +0300

    Lesson 8 Added front login form and settings for roles

--- core-service/src/main/java/ru/gb/jSilver/spring/market/core/controllers/AuthController.java
+++ core-service/src/main/java/ru/gb/jSilver/spring/market/core/controllers/AuthController.java
@@ -7,15 +7,15 @@ import org.springframework.security.authentication.AuthenticationManager;
 import org.springframework.security.authentication.BadCredentialsException;
 import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
 import org.springframework.security.core.userdetails.UserDetails;
-import org.springframework.web.bind.annotation.CrossOrigin;
-import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.*;
 import ru.gb.jSilver.spring.market.api.AppError;
 import ru.gb.jSilver.spring.market.api.AuthResponse;
+import ru.gb.jSilver.spring.market.core.dtos.StringResponse;
 import ru.gb.jSilver.spring.market.core.services.UserService;
 import ru.gb.jSilver.spring.market.core.utils.JwtTokenUtil;
 import ru.gb.jSilver.spring.market.api.AuthRequest;
+
+import java.security.Principal;
 
 @RestController
 @RequiredArgsConstructor
@@ -36,5 +36,11 @@ public class AuthController {
         UserDetails userDetails = userService.loadUserByUsername(authRequest.getUsername());
         String token = jwtTokenUtil.generateToken(userDetails);
         return ResponseEntity.ok(new AuthResponse(token));
+    }
+
+    @GetMapping("/check_roles/{username}")
+    public StringResponse checkRoles(@PathVariable String username) {
+        UserDetails userDetails = userService.loadUserByUsername(username);
+        return new StringResponse(userDetails.getAuthorities().toString());
     }
 }
